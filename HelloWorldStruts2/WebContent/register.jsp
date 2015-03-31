02f36b86d5451131611690fc731ce9dba5d18678
Brian Mc Cabe <mccabebrian1@hotmail.com>
2015-03-31 14:09:12 +0100

    First

---
+++ HelloWorldStruts2/WebContent/register.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix="s" uri="/struts-tags"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" 
+"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>Register</title>
+</head>
+<body>
+   <form action="registeraction" method="post">
+      Name:<br/><input type="text" name="name"/><br/>
+      User:<br/><input type="text" name="user"/><br/>
+      Password:<br/><input type="password" name="password"/><br/>
+      <input type="submit" value="Register"/>		
+   </form>
+</body>
+</html>
---
+++ HelloWorldStruts2/src/com/tutorialspoint/struts2/RegisterAction.java
@@ -0,0 +1,74 @@
+package com.tutorialspoint.struts2;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class RegisterAction extends ActionSupport {
+
+   private String user;
+   private String password;
+   private String name;
+
+   public String execute() {
+      String ret = ERROR;
+      Connection conn = null;
+
+      try {
+         String URL = "jdbc:mysql://localhost/struts_tutorial";
+         Class.forName("com.mysql.jdbc.Driver");
+         conn = DriverManager.getConnection(URL, "root", "root");
+         String sql = "Insert into login (name, user, password) values (?,?,?)";
+         
+         PreparedStatement ps = conn.prepareStatement(sql);
+         ps.setString(1, name);
+         ps.setString(2, user);
+         ps.setString(3, password);
+         ps.executeUpdate();
+
+        
+           
+            ret = SUCCESS;
+        
+      } catch (Exception e) {
+    	System.out.println(e);
+    	password = e.getMessage();
+         ret = ERROR;
+      } finally {
+         if (conn != null) {
+            try {
+               conn.close();
+            } catch (Exception e) {
+            }
+         }
+      }
+      return ret;
+   }
+
+   public String getUser() {
+      return user;
+   }
+
+   public void setUser(String user) {
+      this.user = user;
+   }
+
+   public String getPassword() {
+      return password;
+   }
+
+   public void setPassword(String password) {
+      this.password = password;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+}
